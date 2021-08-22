2d64179697e4281b074e9fce584d20a7eedbb9e4
Adrian Mason <adrianlmason@gmail.com>
2021-08-22 13:07:44 -0500

    Add pics, add Header Links

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
 import useStyles from '@/utils/styles'
 import Head from 'next/head'
+import NextLink from 'next/link'
 
 export default function Layout({ children }) {
   const classes = useStyles()
@@ -11,7 +12,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position='static' className={classes.navbar}>
         <Toolbar>
-          <Typography>amase-zon</Typography>
+          <NextLink href='/' passHref>
+            <Link>
+              <Typography className={classes.brand}>amase-zon</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href='/cart' passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href='/login' passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>
---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'A very popular shirt'
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/shirt2.jpg',
+      price: 80,
+      brand: 'Adidas',
+      rating: 4.2,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'Athletic fit shirt'
+    },
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/shirt3.jpg',
+      price: 90,
+      brand: 'Polo',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'Slim fitting shirt'
+    },
+    {
+      name: 'Golf Pants',
+      category: 'Pants',
+      image: '/images/pants1.jpg',
+      price: 90,
+      brand: 'Dockers',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'Comfortable golf pants'
+    },
+    {
+      name: 'Slim Fit Pants',
+      category: 'Pants',
+      image: '/images/pants2.jpg',
+      price: 95,
+      brand: 'Lucky Brand',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'Most popular slim fitting pants'
+    },
+    {
+      name: 'Classic Pants',
+      category: 'Pants',
+      image: '/images/pants3.jpg',
+      price: 75,
+      brand: 'Wrangler',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'OG pair of pants'
+    },
+  ]
+}
+
+export default data
