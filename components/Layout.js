b0823cb89f1f06a9898008fbc4c70e798f096d22
Adrian Mason <adrianlmason@gmail.com>
2021-08-29 10:19:39 -0500

    Add Material-UI Theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,32 @@
-import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Container, createTheme, CssBaseline, Link, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
 import useStyles from '@/utils/styles'
 import Head from 'next/head'
 import NextLink from 'next/link'
 
 export default function Layout({ title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0'
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0'
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080'
+      }
+    }
+  })
   const classes = useStyles()
   return (
     <div>
@@ -11,30 +34,33 @@ export default function Layout({ title, description, children }) {
         <title>{title ? `${title} - amase-zon` : 'amase-zon'}</title>
         {description && <meta name='descripton' content={description} />}
       </Head>
-      <AppBar position='static' className={classes.navbar}>
-        <Toolbar>
-          <NextLink href='/' passHref>
-            <Link>
-              <Typography className={classes.brand}>amase-zon</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href='/cart' passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>     
+        <CssBaseline />   
+        <AppBar position='static' className={classes.navbar}>
+          <Toolbar>
+            <NextLink href='/' passHref>
+              <Link>
+                <Typography className={classes.brand}>amase-zon</Typography>
+              </Link>
             </NextLink>
-            <NextLink href='/login' passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>
-        {children}
-      </Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Amase-Zon.</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href='/cart' passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href='/login' passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>
+          {children}
+        </Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Amase-Zon.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   )
 }
