c4febdb5d2ed265e038f1a4f0d10dec7c0cd23d3
Adrian Mason <adrianlmason@gmail.com>
2021-10-13 12:19:40 -0500

    Build Login API

--- pages/login.js
+++ pages/login.js
@@ -2,21 +2,34 @@ import Layout from "@/components/Layout";
 import useStyles from "@/utils/styles";
 import NextLink from 'next/link'
 import { Button, Link, List, ListItem, TextField, Typography } from "@material-ui/core";
+import axios from "axios";
+import { useState } from "react";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const classes = useStyles()
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('/api/users/login', { email, password })
+      alert('login successful')
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message)
+    }
+  }
   return (
     <Layout title='login'>
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component='h1' variant='h1'>
           Login
         </Typography>
         <List>
           <ListItem>
-            <TextField variant='outlined' fullWidth id='email' label='Email' inputProps={{ type: 'email' }} />
+            <TextField variant='outlined' fullWidth id='email' label='Email' inputProps={{ type: 'email' }} onChange={e => setEmail(e.target.value)} />
           </ListItem>
           <ListItem>
-            <TextField variant='outlined' fullWidth id='password' label='Password' inputProps={{ type: 'password' }}/>
+            <TextField variant='outlined' fullWidth id='password' label='Password' inputProps={{ type: 'password' }} onChange={e => setPassword(e.target.value)} />
           </ListItem>
           <ListItem>
             <Button variant='contained' type='submit' fullWidth color='primary'>Login</Button>
