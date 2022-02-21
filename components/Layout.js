26f3f67dc8af2dd01303d7e3c594139f4c6ff0f3
Adrian Mason <adrianlmason@gmail.com>
2022-02-21 11:34:23 -0600

    Create Admin Dashboard

--- components/Layout.js
+++ components/Layout.js
@@ -92,6 +92,9 @@ export default function Layout({ title, description, children }) {
                   <Menu id='simple-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={loginMenuCloseHandler}>
                     <MenuItem onClick={e => loginMenuCloseHandler(e, '/profile')}>Profile</MenuItem>
                     <MenuItem onClick={e => loginMenuCloseHandler(e, '/order-history')}>Order History</MenuItem>
+                    {userInfo.isAdmin && (
+                      <MenuItem onClick={e => loginMenuCloseHandler(e, '/admin/dashboard')}>Admin Dashboard</MenuItem>
+                    )}
                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
                   </Menu>
                 </>
