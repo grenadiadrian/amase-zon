2d6536385aa2f2c99e43ed912b5350f9f1fa1c61
Adrian Mason <adrianlmason@gmail.com>
2022-03-04 06:58:41 -0600

    Deliver order for Admin

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -23,11 +23,19 @@ function reducer(state, action) {
     case 'PAY_REQUEST':
       return { ...state, loadingPay: true }
     case 'PAY_SUCCESS':
-      return { ...state, loading: false, successPay: true }
+      return { ...state, loadingPay: false, successPay: true }
     case 'PAY_FAIL':
       return { ...state, loadingPay: false, errorPay: action.payload }
     case 'PAY_RESET':
       return { ...state, loadingPay: false, successPay: false, errorPay: '' }
+    case 'DELIVER_REQUEST':
+      return { ...state, loadingDeliver: true }
+    case 'DELIVER_SUCCESS':
+      return { ...state, loadingDeliver: false, successDeliver: true }
+    case 'DELIVER_FAIL':
+      return { ...state, loadingDeliver: false, errorDeliver: action.payload }
+    case 'DELIVER_RESET':
+      return { ...state, loadingDeliver: false, successDeliver: false, errorDeliver: '' }
     default: state
   }
 }
@@ -40,7 +48,7 @@ function OrderScreen({ params }) {
   const { state } = useContext(Store)
   const { userInfo } = state
 
-  const [{ loading, error, order, successPay }, dispatch] = useReducer(reducer, { loading: true, order: {}, error: '' })
+  const [{ loading, error, order, successPay, loadingDeliver, successDeliver }, dispatch] = useReducer(reducer, { loading: true, order: {}, error: '' })
   const { 
     shippingAddress, 
     paymentMethod, 
@@ -70,10 +78,13 @@ function OrderScreen({ params }) {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err)} )
       }
     }
-    if (!order._id || successPay || order._id && order._id !== orderId) {
+    if (!order._id || successPay || successDeliver || order._id && order._id !== orderId) {
       fetchOrder()
       if (successPay) {
         dispatch({ type: 'PAY_RESET' })
+      }
+      if (successDeliver) {
+        dispatch({ type: 'DELIVER_RESET' })
       }
     } else {
       const loadPayPalScript = async () => {
@@ -88,7 +99,7 @@ function OrderScreen({ params }) {
       }
       loadPayPalScript()
     }
-  }, [order, successPay])
+  }, [order, successPay, successDeliver])
 
   const { closeSnackbar, enqueueSnackbar } = useSnackbar()
 
@@ -124,6 +135,24 @@ function OrderScreen({ params }) {
 
   function onError(err) {
     enqueueSnackbar(getError(err), { variant: 'error' })
+  }
+
+  async function deliverOrderHandler() {
+    try {
+      dispatch({ type: 'DELIVER_REQUEST' })
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}`}
+        }
+      )
+      dispatch({ type: 'DELIVER_SUCCESS', payload: data })
+      enqueueSnackbar('Order delivered!', { variant: 'success' })
+    } catch (err) {
+      dispatch({ type: 'DELIVER_FAIL', payload: getError(err) })
+      enqueueSnackbar(getError(err), { variant: 'error' })
+    }
   }
   
   return (
@@ -271,6 +300,19 @@ function OrderScreen({ params }) {
                         ></PayPalButtons>
                      </div>
                     )}
+                  </ListItem>
+                )}
+                {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                  <ListItem>
+                    {loadingDeliver && <CircularProgress />}
+                    <Button 
+                      fullWidth 
+                      variant='contained' 
+                      color='primary'
+                      onClick={deliverOrderHandler}
+                    >
+                      Deliver Order
+                    </Button>
                   </ListItem>
                 )}
               </List>
