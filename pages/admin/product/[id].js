039b9f68892c142991540134a4f38e8743d3df4a
Adrian Mason <adrianlmason@gmail.com>
2022-03-27 00:42:16 -0500

    Upload Product Image

--- pages/admin/product/[id].js
+++ pages/admin/product/[id].js
@@ -25,6 +25,12 @@ function reducer(state, action) {
       return { ...state, loadingUpdate: false, errorUpdate: '' }
     case 'UPDATE_FAIL':
       return { ...state, loadingUpdate: false, errorUpdate: action.payload }
+    case 'UPLOAD_REQUEST':
+      return { ...state, loadingUpload: true, errorUpload: '' }
+    case 'UPLOAD_SUCCESS':
+      return { ...state, loadingUpload: false, errorUpload: '' }
+    case 'UPLOAD_FAIL':
+      return { ...state, loadingUpload: false, errorUpload: action.payload }
     default:
       return state
   }
@@ -35,7 +41,7 @@ function ProductEditScreen({ params }) {
   const { handleSubmit, control, formState: { errors }, setValue } = useForm()
   const { enqueueSnackbar, closeSnackbar } = useSnackbar()
   const { state } = useContext(Store)
-  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
+  const [{ loading, error, loadingUpdate, loadingUpload }, dispatch] = useReducer(reducer, {
     loading: true,
     error: ''
   })
@@ -69,6 +75,27 @@ function ProductEditScreen({ params }) {
       fetchData()
     }
   }, [])
+
+  const uploadHandler = async (e) => {
+    const file = e.target.files[0]
+    const bodyFormData = new FormData()
+    bodyFormData.append('file', file)
+    try {
+      dispatch({ type: 'UPLOAD_REQUEST' })
+      const { data } = await axios.post('/api/admin/upload', bodyFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          authorization: `Bearer ${userInfo.token}`
+        }
+      })
+      dispatch({ type: 'UPLOAD_SUCCESS' })
+      setValue('image', data.secure_url)
+      enqueueSnackbar('File uploaded successfully!', { variant: 'success' })
+    } catch (err) {
+      dispatch({ type: 'UPLOAD_FAIL' })
+      enqueueSnackbar(getError(err), { variant: 'error' })
+    }
+  }
 
   const submitHandler = async ({ name, slug, price, category, image, brand, countInStock, description }) => {
     closeSnackbar()
@@ -224,7 +251,14 @@ function ProductEditScreen({ params }) {
                           )}
                       >
                       </Controller>
-                    </ListItem>                    
+                    </ListItem>
+                    <ListItem>
+                      <Button variant='contained' component='label'>
+                        Upload File
+                        <input type='file' onChange={uploadHandler} hidden />
+                      </Button>
+                      {loadingUpload && <CircularProgress />}
+                    </ListItem>                  
                     <ListItem>
                       <Controller
                         name="category"
