dce1508b40ddb23c412ff8859c09cfd6d80e1ab9
Adrian Mason <adrianlmason@gmail.com>
2022-03-27 14:17:22 -0500

    Create and Delete Product

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -8,6 +8,7 @@ import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import NextLink from 'next/link'
 import React, { useContext, useEffect, useReducer } from 'react'
+import { useSnackbar } from 'notistack'
 
 function reducer(state, action) {
   switch (action.type) {
@@ -17,6 +18,20 @@ function reducer(state, action) {
       return { ...state, loading: false, products: action.payload, error: '' }
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload }
+    case 'CREATE_REQUEST':
+      return { ...state, loadingCreate: true }
+    case 'CREATE_SUCCESS':
+      return { ...state, loadingCreate: false }
+    case 'CREATE_FAIL':
+      return { ...state, loadingCreate: false }
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true }
+    case 'DELETE_SUCCESS':
+      return { ...state, loadingDelete: false, successDelete: true }
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false }
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false }
     default: state
   }
 }
@@ -27,7 +42,7 @@ function AdminDashboardScreen() {
   const classes = useStyles()
   const { userInfo } = state
 
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, { loading: true, products: [], error: '' })
+  const [{ loading, error, products, loadingCreate, loadingDelete, successDelete }, dispatch] = useReducer(reducer, { loading: true, products: [], error: '' })
 
   useEffect(() => {
     if (!userInfo) {
@@ -44,8 +59,52 @@ function AdminDashboardScreen() {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
     }
-    fetchData()
-  }, [])
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' })
+    } else {
+      fetchData()
+    }
+  }, [successDelete])
+
+  const { enqueueSnackbar } = useSnackbar()
+  const createHandler = async () => {
+    if (!window.confirm('Are you sure?')) {
+      return
+    }
+    try {
+      dispatch({ type: 'CREATE_REQUEST' })
+      const { data } = await axios.post('/api/admin/products', {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` }
+        }
+      )
+      dispatch({ type: 'CREATE_SUCCESS' })
+      enqueueSnackbar('Product created successfully!', { variant: 'success' })
+      router.push(`/admin/product/${data.product._id}`)
+    } catch (err) {
+      dispatch({ type: 'CREATE_FAIL' })
+      enqueueSnackbar(getError(err), { variant: 'error' })
+    }
+  }
+  const deleteHandler = async (productId) => {
+    if (!window.confirm('Are you sure?')) {
+      return
+    }
+    try {
+      dispatch({ type: 'DELETE_REQUEST' })
+      await axios.delete(`/api/admin/products/${productId}`,
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` }
+        }
+      )
+      dispatch({ type: 'DELETE_SUCCESS' })
+      enqueueSnackbar('Product deleted successfully!', { variant: 'success' })
+    } catch (err) {
+      dispatch({ type: 'DELETE_FAIL' })
+      enqueueSnackbar(getError(err), { variant: 'error' })
+    }
+  }
+
   return (
     <Layout title='Product History'>
       <Grid container spacing={1}>
@@ -74,9 +133,18 @@ function AdminDashboardScreen() {
           <Card className={classes.section}>
             <List>
               <ListItem>
-                <Typography component='h1' variant='h1'>
-                  Products
-                </Typography>
+                <Grid container alignItems='center'>
+                  <Grid item xs={6}>
+                    <Typography component='h1' variant='h1'>
+                      Products
+                    </Typography>
+                    {loadingDelete && <CircularProgress />}                    
+                  </Grid>
+                  <Grid align='right' item xs={6}>
+                    <Button onClick={createHandler} color='primary' variant='contained'>Create</Button>
+                    {loadingCreate && <CircularProgress />}                    
+                  </Grid>
+                </Grid>
               </ListItem>
               <ListItem>
                 {loading ? (
@@ -110,7 +178,7 @@ function AdminDashboardScreen() {
                               <NextLink href={`/admin/product/${product._id}`} passHref>
                                 <Button size='small' variant='contained'>Edit</Button>
                               </NextLink>{ ' ' }
-                              <Button size='small' variant='contained'>Delete</Button>
+                              <Button onClick={() => deleteHandler(product._id)} size='small' variant='contained'>Delete</Button>
                             </TableCell>
                           </TableRow>
                         ))}
